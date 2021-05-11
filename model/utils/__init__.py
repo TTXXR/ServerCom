9b61f67386453b5845281e9fd57fccddd1c97685
TTXXR <250699925@qq.com>
2021-05-11 17:44:12 +0800

    __init__

---
+++ model/utils/__init__.py
@@ -0,0 +1,4 @@
+from .data_loader import load_data
+from .seq_data_set import DataSet
+from .activation_layer import activation_layer
+from .data_preprocess import data_preprocess
---
+++ model/utils/initialization.py
@@ -0,0 +1,32 @@
+import os
+from model.utils import load_data
+from model.bert.model import Model
+from model.bert.config import conf
+from model.fcn.model import *
+
+
+def initialization(epoch, batch_size, data_root, save_path, load_path, cache=True, train=True, unity=False):
+    print("Initializing model...")
+    conf['epoch'] = epoch
+    conf['batch_size'] = batch_size
+    conf['save_path'] = save_path
+    conf['load_path'] = load_path
+    if unity:
+        train_source = None
+        test_source = None
+    elif train:
+        train_source = load_data(os.path.join(data_root, "Train"), cache=cache)
+        test_source = load_data(os.path.join(data_root, "Test"), cache=cache)
+    else:
+        train_source = load_data(data_root, cache=cache)
+        test_source = train_source
+    conf['train_source'] = train_source
+    conf['test_source'] = test_source
+
+    # model = Model(**conf)
+    # da_rnn_kwargs = {"batch_size": 64, "T": 10}
+    # _, model = da_rnn(None, 0, .001, da_rnn_kwargs)
+
+    model = Model()
+    print("Model initialization complete.")
+    return model
